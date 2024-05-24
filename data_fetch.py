620d5df12718bb3dcad71f90c076571ca94304be
Mikołaj Zakrzewski <mikizakr@gmail.com>
2024-05-24 22:39:23 +0200

    Modified data fetching script

--- data_fetch.py
+++ data_fetch.py
@@ -1,18 +1,18 @@
 from ucimlrepo import fetch_ucirepo
 
 # fetch dataset
-heart_disease = fetch_ucirepo(id=45)
+dataset = fetch_ucirepo(id=45)
 
 # data (as pandas dataframes)
-X = heart_disease.data.features
-X.to_csv('features.csv')
-y = heart_disease.data.targets
-y.to_csv('targets.csv')
+X = dataset.data.features
+X.to_csv('dataset/features.csv')
+y = dataset.data.targets
+y.to_csv('dataset/targets.csv')
 
 # metadata
-metadata = heart_disease.metadata
+metadata = dataset.metadata
 print(metadata)
 
 # variable information
-variable_information = heart_disease.variables
-variable_information.to_csv('variable_information.csv')
+variable_information = dataset.variables
+variable_information.to_csv('dataset/variable_information.csv')
