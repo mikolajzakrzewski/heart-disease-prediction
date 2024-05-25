c9f037ba166137947bf11664906c4de8cf28868c
Mikołaj Zakrzewski <mikizakr@gmail.com>
2024-05-25 15:07:13 +0200

    Dealt with missing values in the dataset; added an option to perform binary classification

--- main.py
+++ main.py
@@ -10,6 +10,21 @@ from sklearn.metrics import (accuracy_score, precision_score, recall_score, f1_s
 # Prepare training and testing dataframes
 X = pd.read_csv('dataset/features.csv', index_col=0)
 y = pd.read_csv('dataset/targets.csv', index_col=0)
+
+# Deal with missing values
+X = X.fillna(X.mean())
+
+# Ask the user if they want to omit the severity of the disease
+omit_severity = input('Omit the severity of the disease? (y/_): ')
+if omit_severity == 'y':
+    # Omit the severity of the disease by replacing target values 2, 3, and 4 with 1
+    y = y.replace([2, 3, 4], 1)
+    dl = ['Absence', 'Presence']
+else:
+    # Retain the severity of the disease
+    dl = ['Absence', 'Mild', 'Moderate', 'Severe', 'Critical']
+
+# Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, train_size=0.8, random_state=3)
 
 # Create and train the model
@@ -27,7 +42,7 @@ f1 = f1_score(y_true, y_pred, average='weighted', zero_division=0.0)
 # Display the confusion matrix
 stats = f'Accuracy: {accuracy}\nPrecision: {precision}\nRecall: {recall}\nF1: {f1}'
 rcParams.update({'figure.autolayout': True})
-disp = ConfusionMatrixDisplay.from_estimator(clf, X_test, y_test, cmap='Reds', colorbar=False)
+disp = ConfusionMatrixDisplay.from_estimator(clf, X_test, y_test, display_labels=dl, cmap='Reds', colorbar=False)
 disp.ax_.set_title('Confusion Matrix')
 disp.ax_.set_xlabel(disp.ax_.get_xlabel() + '\n\n' + stats)
 plt.show()
@@ -40,6 +55,6 @@ cn = cn.astype(str)
 
 # Visualize the first two tree levels
 plt.figure(figsize=(10, 5), dpi=200)
-tree.plot_tree(clf.estimators_[0], max_depth=2, impurity=False, filled=True,
-               feature_names=fn, class_names=cn, rounded=True, fontsize=8)
+tree.plot_tree(clf.estimators_[0], max_depth=2, filled=True, feature_names=fn, class_names=cn,
+               rounded=True, fontsize=8)
 plt.show()
